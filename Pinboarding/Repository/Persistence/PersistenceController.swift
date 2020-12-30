1d9d03715c9963d34b643bb2fc482cb892c608bf
Otavio Cordeiro <otaviocc@users.noreply.github.com>
2020-12-30 17:50:41 +0100

    Remove deleted tags and bookmarks

--- Pinboarding/Repository/Persistence/PersistenceController.swift
+++ Pinboarding/Repository/Persistence/PersistenceController.swift
@@ -30,19 +30,78 @@ struct PersistenceController {
             container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
         }
 
+        // Adds new bookmarks to Core Data
         updatesPublisher
-            .sink { [container] posts in
-                posts.forEach { post in
-                    do {
-                        try Bookmark.makeBookmark(
-                            from: post,
-                            in: container.viewContext
-                        )
-                    } catch {
-                        print("Something happened: \(error)")
-                    }
-                }
+            .sink { [self] posts in
+                self.addNewBookmarks(posts)
+                self.removeDeletedBookmarks(posts)
+                self.removeUnusedTags()
             }
             .store(in: &cancellables)
+    }
+
+    // MARK: - Private
+
+    private func addNewBookmarks(
+        _ posts: [PostResponse]
+    ) {
+        posts.forEach { post in
+            do {
+                try Bookmark.makeBookmark(
+                    from: post,
+                    in: container.viewContext
+                )
+            } catch {
+                print("Something happened: \(error)")
+            }
+        }
+    }
+
+    private func removeDeletedBookmarks(
+        _ posts: [PostResponse]
+    ) {
+        let hashes = posts.map { $0.hash }
+        let request = NSFetchRequest<Bookmark>(
+            entityName: "Bookmark"
+        )
+        request.predicate = NSPredicate(
+            format: "NOT id IN %@",
+            hashes
+        )
+
+        do {
+            let bookmarksToRemove = try container.viewContext.fetch(
+                request
+            )
+
+            for bookmark in bookmarksToRemove {
+                container.viewContext.delete(bookmark)
+            }
+        } catch {
+            print("Something happened: \(error)")
+        }
+    }
+
+    private func removeUnusedTags(
+    ) {
+        let request = NSFetchRequest<Tag>(
+            entityName: "Tag"
+        )
+
+        request.predicate = NSPredicate(
+            format: "bookmarks.@count == 0"
+        )
+
+        do {
+            let tagsToRemove = try container.viewContext.fetch(
+                request
+            )
+
+            for tag in tagsToRemove {
+                container.viewContext.delete(tag)
+            }
+        } catch {
+            print("Something happened: \(error)")
+        }
     }
 }

7276329a206adf6fac59c52862b256deccbcbdf8
Otavio Cordeiro <otaviocc@users.noreply.github.com>
2022-05-27 20:42:29 +0200

    Add MicroContainer

--- Pinboarding/PinboardingApp.swift
+++ Pinboarding/PinboardingApp.swift
@@ -5,33 +5,33 @@ import Foundation
 
     // MARK: - Properties
 
-    private let container = PinboardingAppContainer()
+    private let environment = PinboardingAppEnvironment()
 
     // MARK: - Public
 
     var body: some Scene {
         WindowGroup {
             MainView()
-                .environmentObject(container.container.resolve(type: PinboardRepository.self))
-                .environmentObject(container.container.resolve(type: SettingsStore.self))
-                .environmentObject(container.container.resolve(type: AnyTokenStore.self))
-                .environmentObject(container.container.resolve(type: SearchStore.self))
+                .environmentObject(environment.repository)
+                .environmentObject(environment.settingsStore)
+                .environmentObject(environment.tokenStore)
+                .environmentObject(environment.searchStore)
                 .environment(
                     \.managedObjectContext,
-                     (container.container.resolve(type: PersistenceServiceProtocol.self) as! PersistenceService).container.viewContext
+                     environment.persistenceService.container.viewContext
                 )
         }
         .commands {
             SidebarCommands()
             BookmarkCommands(
-                repository: container.container.resolve(type: PinboardRepository.self)
+                repository: environment.repository
             )
         }
 
         Settings {
             SettingsView()
-                .environmentObject(container.container.resolve(type: SettingsStore.self))
-                .environmentObject(container.container.resolve(type: AnyTokenStore.self))
+                .environmentObject(environment.settingsStore)
+                .environmentObject(environment.tokenStore)
         }
     }
 }
